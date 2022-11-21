78113107bad2cf0c91f7c2d75a17657aac4dccad
Betsy Kalkwarf <109998791+Betsy-Kalkwarf@users.noreply.github.com>
2022-11-20 17:57:06 -0800

    Add files via upload

--- MechaCarChallenge.RScript.R
+++ MechaCarChallenge.RScript.R
@@ -16,3 +16,16 @@ Suspension <- read.csv('Suspension_Coil.csv', header = TRUE)
 summarize_suspension <- Suspension %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep')
 
 summarize_by_lot <- Suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance_PSI=var(PSI), SD_PSI=sd(PSI), .groups = 'keep')
+
+#Start of Deliverable 3
+?t.test()
+t.test(Suspension$PSI, mu=1500)
+
+Lot1 <- subset(Suspension, Manufacturing_Lot =="Lot1")
+Lot2 <- subset(Suspension, Manufacturing_Lot =="Lot2")
+Lot3 <- subset(Suspension, Manufacturing_Lot =="Lot3")
+
+
+t.test(Lot1$PSI, Suspension$PSI)
+t.test(Lot2$PSI, Suspension$PSI)
+t.test(Lot3$PSI, Suspension$PSI)
